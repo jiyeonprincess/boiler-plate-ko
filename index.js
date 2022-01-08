9db0a52b2137fd356084139836494b263df069b3
jyprincess <kook6430@gmail.com>
2022-01-08 15:06:47 +0900

    비 밀 정 보 보호

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser');
+const config = require('./config/key');
+const { User } = require("./models/User");
+
+//application/s-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+//application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://jiyeon:0503@cluster0.dtbd7.mongodb.net/Cluster0?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true 
 }).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
 
-app.get('/', (req, res) => res.send('만득이는 쫀득쫀득해!'))
+app.get('/', (req, res) => res.send('만득이는 쫀득쫀득해!  뿌잉뿌잉'))
+
+
+app.post('/register', (req, res) => {
+
+//회원가입할때 필요한 정보들을 client에서 가져오면 그것들을 데이터 베이스에 넣어준다
+
+
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+      if(err) return res.json({ success: false, err})
+      return res.status(200).json({
+        success: true
+      })
+    })
+  })
+
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
